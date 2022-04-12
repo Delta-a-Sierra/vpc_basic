58a7390777546299d8f80912fcd667d9b492e521
Dwayne Sutherland <dwayne@Dwaynes-MacBook-Pro.local>
2022-04-12 12:01:58 +0100

    updated module to be able to take subent az

--- main.tf
+++ main.tf
@@ -6,22 +6,29 @@ resource "aws_vpc" "tfb" {
     Name = var.name
   }
 }
+
 resource "aws_internet_gateway" "tfb" {
   vpc_id = aws_vpc.tfb.id
   tags = {
     Name = "${var.name}-igw"
   }
 }
+
 resource "aws_route" "internet_access" {
   route_table_id         = aws_vpc.tfb.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.tfb.id
 }
+
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.tfb.id
   cidr_block              = var.public_subnet
   map_public_ip_on_launch = var.map_public_ip_on_launch
+  
+  availability_zone = var.subnet_az
   tags = {
     Name = "${var.name}-public"
   }
+
 }
+
